6941d7d58b6bf9c1dc1f63488aeccf7b93653749
Owen <xiaotu.dev@gmail.com>
2016-04-03 00:11:48 +0800

    Insert a Node at the Tail of a Linked List v3.0

--- InsertANodeAtTheTailOfALinkedList.java
+++ InsertANodeAtTheTailOfALinkedList.java
@@ -4,16 +4,11 @@ Node Insert(Node head,int data) {
     if (head == null) {
         return newNode;
     } else {
-        if (head.next == null) {
-            head.next = newNode;
-            return head;
-        } else {
-            Node currNode = head;
-            while (currNode.next != null) {
-                currNode = currNode.next;
-            }
-            currNode.next = newNode;
-            return head;
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
         }
+        currNode.next = newNode;
+        return head;
     }
 }
