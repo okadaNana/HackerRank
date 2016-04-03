99f2d769f1db9ca84adb3642cdfd7f3219fb4dd5
Owen <xiaotu.dev@gmail.com>
2016-04-03 16:13:48 +0800

    Insert a node at a specific position in a linked list v3.0

--- InsertANodeAtASpecificPositionInALinkedList.java
+++ InsertANodeAtASpecificPositionInALinkedList.java
@@ -2,22 +2,16 @@ Node InsertNth(Node head, int data, int position) {
 	Node newNode = new Node();
 	newNode.data = data;
 
-    if (head == null) {
-    	return newNode;
-    } else if (position == 0) {
-    	newNode.next = head;
-    	return newNode;
-    } else {
-    	int idx = 0;
-    	Node currNode = head;
-    	while (currNode != null) {
-    		idx++;
-    		if (idx == position) {
-    			newNode.next = currNode.next;
-    			currNode.next = newNode;
-    		}
-    		currNode = currNode.next;
-    	}
-    	return head;
-    }
+	if (position == 0) {
+		newNode.next = head;
+        return newNode;
+	} else {
+		Node targetNode = head;
+		for (int i = 1; i < position; i++) {
+			targetNode = targetNode.next;
+		}
+		newNode.next = targetNode.next;
+		targetNode.next = newNode;
+        return head;
+	}
 }
