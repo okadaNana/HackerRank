1df1be3f4ceed16211d9c09b114c153e346d626a
Owen <xiaotu.dev@gmail.com>
2016-04-03 16:15:39 +0800

    formate code style

--- InsertANodeAtASpecificPositionInALinkedList.java
+++ InsertANodeAtASpecificPositionInALinkedList.java
@@ -1,17 +1,17 @@
 Node InsertNth(Node head, int data, int position) {
-	Node newNode = new Node();
-	newNode.data = data;
+    Node newNode = new Node();
+    newNode.data = data;
 
-	if (position == 0) {
-		newNode.next = head;
-        return newNode;
-	} else {
-		Node targetNode = head;
-		for (int i = 1; i < position; i++) {
-			targetNode = targetNode.next;
-		}
-		newNode.next = targetNode.next;
-		targetNode.next = newNode;
+    if (position == 0) {
+        newNode.next = head;
+    return newNode;
+    } else {
+        Node targetNode = head;
+        for (int i = 1; i < position; i++) {
+            targetNode = targetNode.next;
+        }
+        newNode.next = targetNode.next;
+        targetNode.next = newNode;
         return head;
-	}
+    }
 }
