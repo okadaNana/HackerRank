434c20ad11ea5f9c582839f21bbddf5428676ac0
Owen <xiaotu.dev@gmail.com>
2016-04-03 16:50:26 +0800

    Print in Reverse v2.0

--- PrintInReverse.java
+++ PrintInReverse.java
@@ -1,16 +1,6 @@
 void ReversePrint(Node head) {
-    if (head == null) {
-        // Nothing to do
-    } else {
-        List<Integer> datas = new ArrayList<>();
-        Node currNode = head;
-        do {
-            datas.add(currNode.data);
-            currNode = currNode.next;
-        } while (currNode != null);
-
-        for (int i = datas.size() - 1; i >= 0; i--) {
-            System.out.println(datas.get(i));
-        }
+    if (head != null) {
+        ReversePrint(head.next);
+        System.out.println(head.data);
     }
 }
