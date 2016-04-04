78b5996a5bdc6d8f25e6df50e40c20e8fab95a28
Owen <xiaotu.dev@gmail.com>
2016-04-04 15:59:24 +0800

    Delete duplicate-value nodes from a sorted linked list v2.0

--- DeleteDuplicate-valueNodesFromASortedLinkedList.java
+++ DeleteDuplicate-valueNodesFromASortedLinkedList.java
@@ -1,19 +1,16 @@
 Node RemoveDuplicates(Node head) {
-    if (head == null || head.next == null) {
+    if (head == null) {
         return head;
     }
 
     Node currNode = head;
-    Node preNode = head;
-
-    while (currNode != null) {
-        preNode = currNode;
-        currNode = currNode.next;
-
-        if (currNode != null && currNode.data == preNode.data) {
-            preNode.next = currNode.next;
-            currNode = preNode;
+    while (currNode.next != null) {
+        if (currNode.data == currNode.next.data) {
+            currNode.next = currNode.next.next;
+            continue;
         }
+
+        currNode = currNode.next;
     }
 
     return head;
