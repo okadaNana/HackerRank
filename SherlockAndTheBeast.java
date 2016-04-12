191b4a469d4e0a94ba65501eaceb89b637312d3c
Owen <xiaotu.dev@gmail.com>
2016-04-12 15:43:55 +0800

    Sherlock and The Beast

--- SherlockAndTheBeast.java
+++ SherlockAndTheBeast.java
@@ -1,9 +1,3 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class Solution {
 
     public static void main(String[] args) {
@@ -11,36 +5,29 @@ public class Solution {
         int T = scan.nextInt();
         
         for (int k = 0; k < T; k++) {
-            int input = scan.nextInt();
+            int digitsCount = scan.nextInt();            
             
-            if (input % 5 != 0 || input % 3 != 0) {
-                System.out.println(-1);
-                continue;
-            }
-                      
-            for (int count_5 = input; count_5 >= 0; count_5--) {
-                int count_3 = input - count_5;
+            boolean has = false;
+            for (int digit5 = digitsCount; digit5 >= 0; digit5--) {
+                int digit3 = digitsCount - digit5;
                 
-                if (count_5 % 3 == 0) {
-                    for (int i = 1; i <= count_5; i++) {
+                if (digit5 % 3 == 0 && digit3 % 5 == 0) {
+                    has = true;
+                    
+                    for (int i = 0; i < digit5; i++) {
                         System.out.print(5);
                     }
-                    for (int i = 1; i <= count_3; i++) {
+                    for (int i = 0; i < digit3; i++) {
                         System.out.print(3);
                     }
                     System.out.println();
-                } else if (count_3 % 5 == 0) {
-                    for (int i = 1; i <= count_5; i++) {
-                        System.out.print(5);
-                    }
-                    for (int i = 1; i <= count_3; i++) {
-                        System.out.print(3);
-                    }
-                    System.out.println();
+                    break;
                 }
-            }            
+            }
+            if (!has) {
+                System.out.println(-1);
+            }
         }
-        
         scan.close();
     }
 }
