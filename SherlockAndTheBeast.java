c9c08e2faf943907f42944c8194c97ada31ba735
Owen <xiaotu.dev@gmail.com>
2016-04-13 10:54:57 +0800

    Sherlock and The Beast v2

--- SherlockAndTheBeast.java
+++ SherlockAndTheBeast.java
@@ -1,26 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
 public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int T = scan.nextInt();
         
-        for (int k = 0; k < T; k++) {
-            int digitsCount = scan.nextInt();            
+        for (int t = 0; t < T; t++) {
+            int digitsCount = scan.nextInt();
             
             boolean has = false;
-            for (int digit5 = digitsCount; digit5 >= 0; digit5--) {
-                int digit3 = digitsCount - digit5;
+            for (int digit_5 = digitsCount; digit_5 >= 0; digit_5--) {
+                int digit_3 = digitsCount - digit_5;
                 
-                if (digit5 % 3 == 0 && digit3 % 5 == 0) {
-                    has = true;
-                    
-                    for (int i = 0; i < digit5; i++) {
-                        System.out.print(5);
+                if (digit_5 % 3 == 0 && digit_3 % 5 == 0) {
+                	has = true;
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < digit_5; i++) {
+                        sb.append(5);
                     }
-                    for (int i = 0; i < digit3; i++) {
-                        System.out.print(3);
+                    for (int i = 0; i < digit_3; i++) {
+                        sb.append(3);
                     }
-                    System.out.println();
+                    System.out.println(sb.toString());
                     break;
                 }
             }
@@ -30,4 +36,5 @@ public class Solution {
         }
         scan.close();
     }
+
 }
