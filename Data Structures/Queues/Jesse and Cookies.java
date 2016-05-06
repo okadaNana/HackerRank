47d2d701371a96a12a61b3e578606b47c972213c
Owen <xiaotu.dev@gmail.com>
2016-05-06 16:50:25 +0800

    Update Jesse and Cookies, delete blank space

--- Data Structures/Queues/Jesse and Cookies.java
+++ Data Structures/Queues/Jesse and Cookies.java
@@ -10,40 +10,40 @@ import java.util.regex.*;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int numOfCookies = scan.nextInt();
-		int minReqSweetness = scan.nextInt();
-		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(numOfCookies);
-		for (int i = 0; i < numOfCookies; i++) {
-			priorityQueue.add(scan.nextInt());
-		}
-		scan.close();
-		
-		boolean posibble = true;
-		int count = 0;
-		while (true) {
-			if (priorityQueue.isEmpty()) {
-				posibble = false;
-				break;
-			}
-			
-			int first = priorityQueue.remove();
-			if (first >= minReqSweetness) {
-				break;
-			}
-			if (priorityQueue.isEmpty()) {
-				posibble = false;
-				break;
-			}
-			
-			int second = priorityQueue.remove();
-			int sweetness = first + 2 * second;
-			count++;
-			priorityQueue.add(sweetness);
-		}
-		System.out.println(posibble ? count : -1);
-	}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numOfCookies = scan.nextInt();
+        int minReqSweetness = scan.nextInt();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(numOfCookies);
+        for (int i = 0; i < numOfCookies; i++) {
+            priorityQueue.add(scan.nextInt());
+        }
+        scan.close();
+        
+        boolean posibble = true;
+        int count = 0;
+        while (true) {
+            if (priorityQueue.isEmpty()) {
+                posibble = false;
+                break;
+            }
+            
+            int first = priorityQueue.remove();
+            if (first >= minReqSweetness) {
+                break;
+            }
+            if (priorityQueue.isEmpty()) {
+                posibble = false;
+                break;
+            }
+            
+            int second = priorityQueue.remove();
+            int sweetness = first + 2 * second;
+            count++;
+            priorityQueue.add(sweetness);
+        }
+        System.out.println(posibble ? count : -1);
+    }
 
 }
 
